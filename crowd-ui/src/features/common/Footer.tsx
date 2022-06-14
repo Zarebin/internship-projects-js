674975f5913ddd68c952fb344069473858f18b2d
samane26 <samane.teymurifar@gmail.com>
2022-06-14 11:34:42 +0430

    final- add MUI

--- crowd-ui/src/features/common/Footer.tsx
+++ crowd-ui/src/features/common/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './Footer.scss';
+import { KeyboardArrowRight } from '@mui/icons-material';
 import { FormattedMessage } from 'react-intl';
 import { postData } from '../foodFacts/actions';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import { selectData } from '../foodFacts/FoodFactsSlice';
+import './Footer.scss';
 
 enum FoodFactsType{
   Skip=0,
@@ -23,7 +24,7 @@ function Footer() {
     <div className="Footer">
       <button className="Footer__button" type="button" onClick={() => { dispatch(postData(getSendData(FoodFactsType.Skip))); }}>
         <span><FormattedMessage id="app.footer.skip" defaultMessage="English" /></span>
-        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="gray" d="m10 20l8-8l-8-8l-1.414 1.414L15.172 12l-6.586 6.586z" /></svg>
+        <KeyboardArrowRight />
       </button>
     </div>
   );
--- crowd-ui/src/features/common/Header.tsx
+++ crowd-ui/src/features/common/Header.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import './Header.scss';
 import { FormattedMessage } from 'react-intl';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 function Header() {
   return (
     <div className="Header">
       <div className="Header__Back">
-        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24" height="24" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="gray" d="M12.707 17.293L8.414 13H18v-2H8.414l4.293-4.293l-1.414-1.414L4.586 12l6.707 6.707z" /></svg>
-        {/* <p><FormattedMessage id="app.header.lang" defaultMessage="English" /></p> */}
-        {/* <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24" height="24" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="gray" d="m7 10l5 5l5-5z" /></svg> */}
+        <ArrowBackIcon />
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24" height="24" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
-        <circle cx="16" cy="8" r="2" fill="gray" />
-        <circle cx="16" cy="16" r="2" fill="gray" />
-        <circle cx="16" cy="24" r="2" fill="gray" />
-      </svg>
+      <MoreVertIcon />
     </div>
   );
 }
